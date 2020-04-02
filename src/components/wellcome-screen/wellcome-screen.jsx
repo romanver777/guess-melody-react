27a0f74c21e815873234d19e813007185edb5ab7
romanver <romanver777@yandex.ru>
2020-04-02 21:03:32 +0300

    добавлены props, тесты jest и enzyme

--- src/components/wellcome-screen/wellcome-screen.jsx
+++ src/components/wellcome-screen/wellcome-screen.jsx
@@ -1,22 +1,32 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const WellcomeScreen = () => {
+const WellcomeScreen = (props) => {
 
+  const {time, mistakes, onClick} = props;
   return (
     <section className="welcome">
       <div className="welcome__logo">
         <img src="img/melody-logo.png" alt="Угадай мелодию" width="186" height="83"/>
       </div>
-      <button className="welcome__button"><span className="visually-hidden">Начать игру</span></button>
+      <button className="welcome__button"
+        onClick={onClick}
+      ><span className="visually-hidden">Начать игру</span></button>
       <h2 className="welcome__rules-title">Правила игры</h2>
       <p className="welcome__text">Правила просты:</p>
       <ul className="welcome__rules-list">
-        <li>За 5 минут нужно ответить на все вопросы.</li>
-        <li>Можно допустить 3 ошибки.</li>
+        <li>За {time} минут нужно ответить на все вопросы.</li>
+        <li>Можно допустить {mistakes} ошибки.</li>
       </ul>
       <p className="welcome__text">Удачи!</p>
     </section>
   );
+};
+
+WellcomeScreen.propTypes = {
+  time: PropTypes.number.isRequired,
+  mistakes: PropTypes.number.isRequired,
+  onClick: PropTypes.func.isRequired,
 };
 
 export default WellcomeScreen;
--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './components/app/app.jsx';
 const init = () => {
 
   ReactDom.render(
-      <App />,
+      <App/>,
       document.getElementById(`root`)
   );
 };
