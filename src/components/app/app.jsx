fb8fc06e03f5ef206acce1d7eaa841dab73bb2a5
romanver <romanver777@yandex.ru>
2020-04-02 13:42:32 +0300

    first commit

---
+++ src/components/app/app.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import WellcomeScreen from '../wellcome-screen/wellcome-screen.jsx';
+
+const App = () => {
+
+  return <WellcomeScreen />;
+};
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import App from './components/app/app.jsx';
+
+const init = () => {
+
+  ReactDom.render(
+      <App />,
+      document.getElementById(`root`)
+  );
+};
+
+init();
