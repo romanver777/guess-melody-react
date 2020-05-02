7d041234b332a335becdfcc5c93f8984e7addae5
romanver <romanver777@yandex.ru>
2020-05-02 22:52:24 +0300

    добавлен таймер

--- src/index.js
+++ src/index.js
@@ -9,21 +9,19 @@ import getQuests from './helpers/helpers';
 import {reducer} from './reducer';
 
 const gameSettings = {
-  gameTime: 5,
   maxMistakes: 3,
   numberQuests: 10,
 };
 
 const init = (props) => {
 
-  const {gameTime, maxMistakes, numberQuests} = gameSettings;
+  const {maxMistakes, numberQuests} = gameSettings;
   const quests = getQuests(props, numberQuests);
   const store = createStore(reducer);
 
   ReactDom.render(
     <Provider store={store}>
       <App
-        time={gameTime}
         maxMistakes={maxMistakes}
         quests={quests}
       />
